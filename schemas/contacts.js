7d7e4d1f0bd61a282a5609d454aee8f52a4e0647
Mozzhakov <imozzhakov@icloud.com>
2023-06-10 09:55:02 +0300

    Update contacts.js

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
-    .pattern(/\(.*\) .*-/) // number format (xxx) xxx-xxxx
+    // .pattern(/\(.*\) .*-/) // number format (xxx) xxx-xxxx
     .max(14)
     .required(),
 });
